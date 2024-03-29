44b29e30f269bdd3b1185f4204a28803e6720d90
Mounika-chukkala <126870495+Mounika-chukkala@users.noreply.github.com>
2024-03-29 11:40:04 +0530

    Add files via upload

---
+++ innovation/innovation-gallery.js
@@ -0,0 +1,11 @@
+const logos=document.querySelectorAll(".logo")
+
+logos.forEach(logo=>{
+logo.addEventListener("click",()=>{
+    const liked=logo.querySelector(".liked")
+    const unliked=logo.querySelector(".unliked")
+    liked.classList.toggle("toggle");
+    unliked.classList.toggle("toggle");
+    
+})
+});
